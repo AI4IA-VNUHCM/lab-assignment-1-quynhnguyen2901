d3ca5125a5b8467b93727d918278487d34d1a84b
quynhnguyen2901 <91877086+quynhnguyen2901@users.noreply.github.com>
2021-10-05 00:54:23 +0700

    update ex.3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -11,11 +11,45 @@ Ex:
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-int main(int argc, char *argv[]) {
+int isGreaterThan2(int x)
+{
+	if(x<2){
+		return 0;
+	}
+	
+    return 1;
+	
+}
+int main(int argc, char *argv[]){
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	//Your codes here
+	while (testcase!=0)
+	{
+		for(int so=0;so<testcase;so++){
+			if(isGreaterThan2(so)==1){
+
+				int count = 0;
+				for(int i = 2; i <= sqrt(so); i++){
+					if(so % i == 0){
+						count++;
+					}
+				}
+				if(count == 0){
+					printf("\n%d",so);
+				}
+			
+			}
+			
+			
+
+		}
+		return 0;
 	
+		
+	}
 	return 0;
+	
 }
+
+
